4d085afd00fc1f74ee66cb88445b7b130dca435c
FabioMiguelNascimento <fabiomnascimento05@gmail.com>
2025-12-11 16:58:59 -0300

    feat: increase api-client timeout

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -4,7 +4,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 10000,
+  timeout: 60000,
   withCredentials: false,
 });
 
